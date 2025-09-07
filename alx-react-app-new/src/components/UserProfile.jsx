d92cbbf1989cc9ba282a885d3d3e6c164279dbb5
Williams Ameyaw <williams.kyere@stu.ucc.edu.gh>
2025-09-07 18:43:27 -0400

    edited userprofile

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,13 +1,41 @@
+const UserProfile = (props) => {
+  return (
+    <div
+      style={{
+        border: "2px solid blue",
+        padding: "15px",
+        borderRadius: "8px",
+      }}
+    >
+      <h2
+        style={{
+          fontSize: "28px",
+          marginTop: "20px",
+          color: "gray",
+        }}
+      >
+        <span>{props.name}</span>
+      </h2>
+      <p
+        style={{
+          fontSize: "17px",
+          marginTop: "10px",
+          color: "white",
+        }}
+      >
+        Age: {props.age}
+      </p>
+      <p
+        style={{
+          fontSize: "20px",
+          marginTop: "20px",
+          color: "green",
+        }}
+      >
+        Bio: {props.bio}
+      </p>
+    </div>
+  );
+};
 
-  const UserProfile = (props) => {
-   return (
-     <div>
-       <h2 style={{fontSize: "28px", marginTop: "20px", color: "gray"}}>{props.name}</h2>
-       <p style={{fontSize: "17px", marginTop: "10px", color: "white"}}>Age: {props.age}</p>
-       <p style={{fontSize: "20px", marginTop: "20px", color: "green"}}>Bio: {props.bio}</p>
-     </div>
-   );
- };
-
- export default UserProfile;
-  
+export default UserProfile;
