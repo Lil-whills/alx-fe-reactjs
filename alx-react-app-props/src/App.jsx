d2e731e7ebaf12be7ac03d2e47f1caebb9566ad3
Williams Ameyaw <williams.kyere@stu.ucc.edu.gh>
2025-09-07 20:23:46 -0400

    last commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,6 @@
-import UserContext from "./UserContext";
+import UserContext from "./components/UserContext";
 import UserProfile from "./components/UserProfile";
+
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
