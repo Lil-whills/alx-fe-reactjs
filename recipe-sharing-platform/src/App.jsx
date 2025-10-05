316dfee3194d086a8a7feb092240c0449071ecdb
Williams Ameyaw <williams.kyere@stu.ucc.edu.gh>
2025-10-05 01:13:50 -0400

    added recipe-sharing-platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="text-5xl font-bold text-blue-600">
+        Tailwind is Working! 🎉
+      </div>
+    </div>
+  );
+}
+
+export default App;
